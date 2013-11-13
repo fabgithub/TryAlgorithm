d32dfd15b641dc52bf0d9e1cdf695f7a675775bb
lyd_fab <lyd_fab_subscribe@126.com>
2013-11-13 16:53:34 +0800

    添加杨辉三角变形算法.

---
+++ YangHuiExtend/YangHuiExtend/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  YangHuiExtend
+//
+//  Created by liyoudi on 20131113.
+//  Copyright (c) 2013年 liyoudi. All rights reserved.
+//
+
+#include <iostream>
+#include <map>
+#include <set>
+
+// 判断在n, m位置是否是奇数.
+bool IsOdd(int n, int m);
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
