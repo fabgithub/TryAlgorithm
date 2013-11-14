3e245130910af04a0dd3bd304db5e866ae251639
lyd_fab <lyd_fab_subscribe@126.com>
2013-11-14 11:40:53 +0800

    实现第一版BeautifuString, 这版尚有问题。

--- BeautifulString/BeautifulString/BeautifulString.cpp
+++ BeautifulString/BeautifulString/BeautifulString.cpp
@@ -7,3 +7,53 @@
 //
 
 #include "BeautifulString.h"
+
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+
+int perfect(const string &strOrign)
+{
+    string s;
+    const char *szCh = strOrign.c_str();
+    while (*szCh)
+    {
+        s += *szCh;
+        ++szCh;
+    }
+    map<char,int> chcount;
+    for(int i=0;i<s.length();i++)
+    {
+        char c = (s.at(i));
+        if(c >= 'a' && c <= 'z')
+        {
+            c -= 'a' -'z';
+        }
+        if(chcount.find(c) == chcount.end())
+            chcount[c]=0;
+        chcount[c]++;
+    }
+    multimap<int,char> sortc;
+    map<char,int>::iterator ici = chcount.begin();
+    for(;ici!= chcount.end();++ici)
+    {
+        sortc.insert(std::make_pair(ici->second,ici->first));
+    }
+    int hao = 26;
+    multimap<int,char>::iterator iic = sortc.begin();
+    for(;iic != sortc.end();++iic)
+    {
+        chcount[iic->second] = hao;
+        hao--;
+    }
+    hao=0;
+    for(int i=0;i<s.length();i++)
+    {
+        hao += chcount[s.at(i)];
+    }
+    return hao;
+}
