d196dfa7d8d82103ceb9d73a52f71d03a6f24c66
lyd_fab <lyd_fab_subscribe@126.com>
2013-11-15 15:56:11 +0800

    实现CoverageNumber，并且进行了测试用例的编写。

---
+++ CoverageNumber/CoverageNumber/CoverageNumber.h
@@ -0,0 +1,16 @@
+//
+//  CoverageNumber.h
+//  CoverageNumber
+//
+//  Created by liyoudi on 20131115.
+//  Copyright (c) 2013年 liyoudi. All rights reserved.
+//
+
+#ifndef __CoverageNumber__CoverageNumber__
+#define __CoverageNumber__CoverageNumber__
+
+#include <iostream>
+
+int CoverageNumber(int a, int b, int x, int y);
+
+#endif /* defined(__CoverageNumber__CoverageNumber__) */
