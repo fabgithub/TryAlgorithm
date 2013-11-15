b402607e156d34ae0652d5a248e35bb038d0f7da
lyd_fab <lyd_fab_subscribe@126.com>
2013-11-15 17:39:19 +0800

    初步添加素数获取方法。

---
+++ LuckNumber/LuckNumber/LuckNumber.h
@@ -0,0 +1,16 @@
+//
+//  LuckNumber.h
+//  LuckNumber
+//
+//  Created by liyoudi on 20131115.
+//  Copyright (c) 2013年 liyoudi. All rights reserved.
+//
+
+#ifndef __LuckNumber__LuckNumber__
+#define __LuckNumber__LuckNumber__
+
+#include <iostream>
+
+int lucky(int x, int y);
+
+#endif /* defined(__LuckNumber__LuckNumber__) */
